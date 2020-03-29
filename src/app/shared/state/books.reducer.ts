9fb010e48c2c57e669ee4833044a7f413e3c61cc
Mike Ryan <mike.ryan52@gmail.com>
2020-03-28 19:57:25 -0500

    05-reduce-api-actions

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -33,6 +33,30 @@ export const booksReducer = createReducer(
       ...state,
       activeBookId: action.bookId
     };
+  }),
+  on(BooksApiActions.booksLoaded, (state, action) => {
+    return {
+      ...state,
+      collection: action.books
+    };
+  }),
+  on(BooksApiActions.bookCreated, (state, action) => {
+    return {
+      collection: createBook(state.collection, action.book),
+      activeBookId: null
+    };
+  }),
+  on(BooksApiActions.bookUpdated, (state, action) => {
+    return {
+      collection: updateBook(state.collection, action.book),
+      activeBookId: null
+    };
+  }),
+  on(BooksApiActions.bookDeleted, (state, action) => {
+    return {
+      ...state,
+      collection: deleteBook(state.collection, action.bookId)
+    };
   })
 );
 
