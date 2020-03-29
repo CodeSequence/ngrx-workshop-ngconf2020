d76c1173a22d072261faba6a35c4661e06273690
Mike Ryan <mike.ryan52@gmail.com>
2020-03-28 19:54:31 -0500

    03-reducers

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -1,3 +1,37 @@
 import { createReducer, on, Action, createSelector } from "@ngrx/store";
 import { BookModel, calculateBooksGrossEarnings } from "src/app/shared/models";
 import { BooksPageActions, BooksApiActions } from "src/app/books/actions";
+
+const createBook = (books: BookModel[], book: BookModel) => [...books, book];
+const updateBook = (books: BookModel[], changes: BookModel) =>
+  books.map(book => {
+    return book.id === changes.id ? Object.assign({}, book, changes) : book;
+  });
+const deleteBook = (books: BookModel[], bookId: string) =>
+  books.filter(book => bookId !== book.id);
+
+export interface State {
+  collection: BookModel[];
+  activeBookId: string | null;
+}
+
+export const initialState: State = {
+  collection: [],
+  activeBookId: null
+};
+
+export const booksReducer = createReducer(
+  initialState,
+  on(BooksPageActions.clearSelectedBook, BooksPageActions.enter, state => {
+    return {
+      ...state,
+      activeBookId: null
+    };
+  }),
+  on(BooksPageActions.selectBook, (state, action) => {
+    return {
+      ...state,
+      activeBookId: action.bookId
+    };
+  })
+);
