7661d3a971935842627e9f2d7c3052f751b3242e
Mike Ryan <mike.ryan52@gmail.com>
2020-03-28 20:06:30 -0500

    10-more-effects

--- src/app/books/books-api.effects.ts
+++ src/app/books/books-api.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from "@ngrx/effects";
-import { mergeMap, map } from "rxjs/operators";
+import { mergeMap, map, exhaustMap, concatMap } from "rxjs/operators";
 import { BooksService } from "../shared/services";
 import { BooksPageActions, BooksApiActions } from "./actions";
 
@@ -11,10 +11,45 @@ export class BooksApiEffects {
   loadBooks$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BooksPageActions.enter),
-      mergeMap(() =>
+      exhaustMap(() =>
         this.booksService
           .all()
           .pipe(map(books => BooksApiActions.booksLoaded({ books })))
+      )
+    )
+  );
+
+  createBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.createBook),
+      concatMap(action =>
+        this.booksService
+          .create(action.book)
+          .pipe(map(book => BooksApiActions.bookCreated({ book })))
+      )
+    )
+  );
+
+  updateBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.updateBook),
+      concatMap(action =>
+        this.booksService
+          .update(action.bookId, action.changes)
+          .pipe(map(book => BooksApiActions.bookUpdated({ book })))
+      )
+    )
+  );
+
+  deleteBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.deleteBook),
+      mergeMap(action =>
+        this.booksService
+          .delete(action.bookId)
+          .pipe(
+            map(() => BooksApiActions.bookDeleted({ bookId: action.bookId }))
+          )
       )
     )
   );
