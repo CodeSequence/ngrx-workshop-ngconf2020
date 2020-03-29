e22296e31f50ec79bceac226e335052be8f89c9e
Mike Ryan <mike.ryan52@gmail.com>
2020-03-28 20:19:55 -0500

    17-auth-effects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from "@angular/core";
 import { EffectsModule } from "@ngrx/effects";
 import { LoginPageComponentModule } from "./components/login-page";
 import { UserComponentModule } from "./components/user";
+import { AuthEffects } from "./auth.effects";
 
 @NgModule({
+  imports: [EffectsModule.forFeature([AuthEffects])],
   exports: [LoginPageComponentModule, UserComponentModule]
 })
 export class AuthModule {}
