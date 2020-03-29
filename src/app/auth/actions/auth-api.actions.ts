35c86c2e22f1a01ac7f51e005f5ce75f8c907445
Mike Ryan <mike.ryan52@gmail.com>
2020-03-28 20:18:41 -0500

    16-auth-api-actions

--- src/app/auth/actions/auth-api.actions.ts
+++ src/app/auth/actions/auth-api.actions.ts
@@ -1,2 +1,17 @@
 import { createAction } from "@ngrx/store";
 import { UserModel } from "src/app/shared/models";
+
+export const getAuthStatusSuccess = createAction(
+  "[Auth/API] Get Auth Status Success",
+  (user: UserModel | null) => ({ user })
+);
+
+export const loginSuccess = createAction(
+  "[Auth/API] Login Success",
+  (user: UserModel) => ({ user })
+);
+
+export const loginFailure = createAction(
+  "[Auth/API] Login Failure",
+  (reason: string) => ({ reason })
+);
